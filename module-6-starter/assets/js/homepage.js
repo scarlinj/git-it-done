539ad97ce14256c2fb7cc765d4eb8419506a41b1
Stephen Carlin <scarlinj@gmail.com>
2022-01-25 15:46:58 -0800

    updated gitUserRepos with dynamic request

--- module-6-starter/assets/js/homepage.js
+++ module-6-starter/assets/js/homepage.js
@@ -1,6 +1,28 @@
-var getUserRepos = function () {
-    fetch("https://api.github.com/users/octocat/repos");
-    console.log("function was called");
+var getUserRepos = function (user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function (response) {
+        response.json().then(function (data) {
+            console.log(data);
+        });
+    });
 };
+
+// from 6.1.2-6.1.4
+// var getUserRepos = function () {
+//     fetch("https://api.github.com/users/octocat/repos").then(function (response) {
+//         response.json().then(function (data) {
+//             console.log(data);
+//         })
+//         console.log("inside", response);
+//     });
+
+//     console.log("outside");
+
+//     var response = fetch("https://api.github.com/users/octocat/repos");
+//     console.log(response);
+// };
 
 getUserRepos();
