772f40dac6230c171ba98a45e5421811d1f45cfd
Stephen Carlin <scarlinj@gmail.com>
2022-02-04 22:26:52 -0800

    converted fetched data into DOM elements

--- module-6-starter/assets/js/single.js
+++ module-6-starter/assets/js/single.js
@@ -1,4 +1,17 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+
+var displayWarning = function (repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
 
 var getRepoIssues = function (repo) {
     var apiUrl = "https://api.github.com/repos/" +
@@ -11,6 +24,12 @@ var getRepoIssues = function (repo) {
                 // pass response data to DOM function by replacing console.log
                 // console.log(data);
                 displayIssues(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    // console.log("repo has more than 30 issues");
+                    displayWarning(repo);
+                }
             });
         } else {
             console.log(response);
@@ -57,4 +76,5 @@ var displayIssues = function (issues) {
     }
 }
 
-getRepoIssues("scarlinj/taskinator");
+
+getRepoIssues("yahoo/react");
